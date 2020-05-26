34d9d203cd6f6cbc399e86731ba959cefefa8989
Namratha-Jacob <30745556+Namratha-Jacob@users.noreply.github.com>
2020-05-26 19:32:33 +0400

    Add files via upload

---
+++ skillscore.py
@@ -0,0 +1,68 @@
+# **************************************************************************************************************************
+''' 
+using with gives file closed problem. use a code to obtain skills from skills.csv without using with --- open --- as ---. 
+use pandas from chrome
+'''
+#****************************************************************************************************************************
+
+
+from pyresparser import ResumeParser
+import pandas as pd
+import spacy
+import csv
+import re
+
+data = ResumeParser('Safia.pdf').get_extracted_data()
+# data contains a dictionary
+
+name = data['name']
+skills = data['skills']
+skill1 = str(skills)
+skill = re.sub("[\']", '', skill1)
+skill = skill.split(",")
+
+#removing [
+a = skill[0]
+c = ""
+for i in range(1,len(a)):
+	c+=a[i]
+skill[0] = c
+
+#removing ]
+a = skill[-1]
+c = ""
+for i in range(len(a)-1):
+	c+=a[i]
+skill[-1] = c
+
+
+
+# converting all to lower case
+for i in range(len(skill)):
+	skill[i] = skill[i].lower()
+
+'''
+#removing starting whitespace
+for i in range(len(skill)):
+	skill[i] = skill[i].strip()
+'''
+	
+	
+f = open("requirement.txt","r")
+text = f.read()
+text = text.split(",")
+
+for i in range(len(text)):
+	text[i] = text[i].lower()
+
+score = 0
+for s in text:
+	print("Entered for")
+	if s in skill:
+		print("entered if")
+		score = score + 1
+
+print("Name: ",name)
+print("score: ",score)
+print("requirements: ",text)
+print("skills: ",skill)
